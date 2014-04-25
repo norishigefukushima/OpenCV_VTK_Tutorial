e3064b992ba680cddf142970bd94652e5d9fbe84
Norishige Fukushima <fukushima@nitech.ac.jp>
2014-04-25 12:26:29 +0900

    upload rep

---
+++ OpenCV_VTK_Tutorial/main.cpp
@@ -0,0 +1,104 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/viz/viz3d.hpp>
+using namespace cv;
+
+#define CV_VERSION_NUMBER CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
+
+
+#ifdef _DEBUG
+#pragma comment(lib, "opencv_viz"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_videostab"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_video"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_ts"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_superres"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_stitching"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_photo"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_ocl"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_objdetect"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_nonfree"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_ml"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_legacy"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_imgproc"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_highgui"CV_VERSION_NUMBER"d.lib")
+//#pragma comment(lib, "opencv_haartraining_engine.lib")
+#pragma comment(lib, "opencv_gpu"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_flann"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_features2d"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_core"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_contrib"CV_VERSION_NUMBER"d.lib")
+#pragma comment(lib, "opencv_calib3d"CV_VERSION_NUMBER"d.lib")
+#else
+#pragma comment(lib, "opencv_viz"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_videostab"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_video"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_ts"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_superres"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_stitching"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_photo"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_ocl"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_objdetect"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_nonfree"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_ml"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_legacy"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_imgproc"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_highgui"CV_VERSION_NUMBER".lib")
+//#pragma comment(lib, "opencv_haartraining_engine.lib")
+#pragma comment(lib, "opencv_gpu"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_flann"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_features2d"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_core"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_contrib"CV_VERSION_NUMBER".lib")
+#pragma comment(lib, "opencv_calib3d"CV_VERSION_NUMBER".lib")
+#endif
+
+int main(int argc, char** argv)
+{
+	//ビルドインフォメーションの表示
+	std::cout<<getBuildInformation();
+    
+	/// ウィンドウの作成
+    viz::Viz3d myWindow("Coordinate Frame");
+
+    /// ３色の３次元座標軸をセット
+    myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
+
+    /// (1,1,1) ベクトル方向へ白いラインをセット
+    viz::WLine axis(Point3f(-1.0f,-1.0f,-1.0f), Point3f(1.0f,1.0f,1.0f));
+    axis.setRenderingProperty(viz::LINE_WIDTH, 4.0);
+    myWindow.showWidget("Line Widget", axis);
+
+    /// 立方体を表示
+    viz::WCube cube_widget(Point3f(0.5,0.5,0.0), Point3f(0.0,0.0,-0.5), true, viz::Color::blue());
+    cube_widget.setRenderingProperty(viz::LINE_WIDTH, 4.0);
+
+    /// ディスプレイにセットしたものを表示
+    myWindow.showWidget("Cube Widget", cube_widget);
+
+    /// 回転ベクトルの設定
+    Mat rot_vec = Mat::zeros(1,3,CV_32F);
+    float translation_phase = 0.0, translation = 0.0;
+    while(!myWindow.wasStopped())
+    {
+        /* Rotation using rodrigues */
+        /// Rotate around (1,1,1)
+        rot_vec.at<float>(0,0) += CV_PI * 0.01f;
+        rot_vec.at<float>(0,1) += CV_PI * 0.01f;
+        rot_vec.at<float>(0,2) += CV_PI * 0.01f;
+
+        /// Shift on (1,1,1)
+        translation_phase += CV_PI * 0.01f;
+        translation = sin(translation_phase);
+
+        Mat rot_mat;
+        Rodrigues(rot_vec, rot_mat);
+
+        /// Construct pose
+        Affine3f pose(rot_mat, Vec3f(translation, translation, translation));
+
+        myWindow.setWidgetPose("Cube Widget", pose);
+
+        myWindow.spinOnce(1, true);
+    }
+
+    return 0;
+}
